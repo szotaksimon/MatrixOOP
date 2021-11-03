0f9fb50de0650917b47aef02a138071ad445a1a4
Szoták Simon <szotak.simon16@gmail.com>
2021-11-03 15:59:42 +0100

    main nm létrehozás

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -8,6 +8,12 @@ public class Main {
         System.out.println("A mátrix mérete: " + m.getSorokSzama() + " x " + m.getOszlopokSzama() + "\n\n" + m);
         System.out.println("Elemek összege: " + m.elemekOsszege());
         System.out.println("Pozitív elemek száma: " + m.pozitivElemekDarabszama());
-        System.out.println("Legnagyobb elem: " + m.legnagyobbElem());
+        System.out.println("Legnagyobb elem: " + m.legnagyobbElem() + "\n\n");
+
+        NegyzetesMatrix nm = new NegyzetesMatrix();
+        System.out.println("Négyzetes mátrix: \n" + nm + "\n");
+        System.out.println("Főátló összege: " + nm.foatloOsszege());
+        System.out.println("Mellékátló legnagyobb osszege: " + nm.mellekatloLegnagyobbOsszege());
+
     }
 }
